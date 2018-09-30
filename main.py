3fda43eb722905f88d52cfe6fb5d6c48cbd80f16
Duane Reinhardt <dwreinhardt75@outlook.com>
2018-09-29 20:18:30 -0400

    tested SAT, assignment complete

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 #import items
 from flask import Flask, request, redirect, render_template
-import cgi
-import os
-import jinja2
-import re
+import re #used for email format validation string
 #*****************************
-
-# Using Jinja2 for Templates
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(template_dir), autoescape=True) 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/user_signup')
+@app.route('/')
 def index():
-    template = jinja_env.get_template('form.html')
-    return template.render()
+    return render_template('form.html')
 
 #user input validation functions
     #blank fields
@@ -35,17 +26,8 @@ def valid_user_entry(text):
         return False
     return True
 
-
-# Define Error Codes
-
-# Display Errors
-
-# Complete signup form
-
-# Comlete the task
-
 #return errors or clear everything
-@app.route('/user_signup', methods=['POST'])
+@app.route('/', methods=['POST'])
 def user_signup():
 
     #intake user's inputs from form
@@ -59,7 +41,7 @@ def user_signup():
     username_input_error = "Username must be 3-20 alpa characters only"
     password_input_error = "Password must be 3-20 characters only, no spaces"
     password_mismatch_error = "Passwords do not match"
-    email_invalid = "Please enter a valide email address (ie. you@somewhere.com)"
+    email_invalid = "Please enter a valid email address (ie. you@somewhere.com)"
 
     #assign Error variables
     username_error = ''
@@ -67,59 +49,54 @@ def user_signup():
     verify_password_error = ''
     email_error = ''
 
-#fail checks
-# username
+#FAIL CHECKS
+# username: field will either be empty or invalid...it cannot be both
     if not is_empty(username):                
         username_error = empty_field_error
-        return username_error
-    
+
     if not valid_user_entry(username):
         username_error = username_input_error
-        return username_error
 
-#password
+#password: field will either be empty or invalid...it cannot be both
     if not is_empty(password):
         password_error = empty_field_error
-        return password_error
 
     if not valid_user_entry(password):
         password_error = password_input_error
-        return password_error
 
-#verify password
+#verify password: field will either be empty or invalid...it cannot be both
     if not is_empty(verify_password):
        verify_password_error = empty_field_error
-       return verify_password_error
 
     if not valid_user_entry(verify_password):
         verify_password_error = password_input_error
-        return password_error
 
+#ensure both submitted passwords are identical
     if password != verify_password:
-        return password_mismatch_error
+        verify_password_error = password_mismatch_error
 
-        #verify email syntax
-    if email != '':
-        match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
-        if match == None:
-            email_error = email_invalid
-            return email_error
-        else:
-            return email_error
+#verify email syntax: taken from web source after searching for a common email validation function
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+    
+    if email != '' and match == None:
+        email_error = email_invalid
 
-
-    # Resolve conflicts and redirect or direct user to correct
+# Resolve conflicts and redirect or direct user to correct
     if not username_error and not password_error and not verify_password_error and not email_error:
         username = username
-        return redirect('/welcome?username={0}'.format(username))
+        return welcome(username)
     else:
-        template = jinja_env.get_template('form.html')
-        return template.render(
-        username = username,
-        username_error = username_error,
-        password_error = password_error,
-        verify_password_error = verify_password_error,
-        email_error = email_error
-        )
+        return render_template('form.html',
+            username = username,
+            username_error = username_error,
+            password_error = password_error,
+            verify_password_error = verify_password_error,
+            email_error = email_error
+            )
+
+#Successful sign-up!!
+def welcome(text):
+    username = text
+    return render_template('welcome.html', username = username)
 
 app.run()
